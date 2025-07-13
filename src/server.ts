0fdaf240f85047f92c7c6161521f11c9b4def502
Owbird <owbirdphorccuo@gmail.com>
2025-07-13 19:53:07 +0000

    chore: Setup db

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI as string)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
 
 const app = express();
 const PORT = 3000;
